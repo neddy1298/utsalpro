56a38e95c46aaea9612bfcd037908d06545b9e12
Neddy AP <neddy1298@gmail.com>
2022-11-24 09:43:34 +0700

    UTS Algoritma Pemrograman

---
+++ Variable.java
@@ -0,0 +1,11 @@
+public class Variable {
+
+    public static void main(String[] args) {
+        String nama = "Neddy Avgha Prasetio";
+        Integer tahun = 2001;
+
+        System.out.println(("Assalamu'alaikum"));
+        System.out.println(("Nama Saya : " + nama));
+        System.out.println(("Saya Lahir di Tahun : " + tahun));
+    }
+}
